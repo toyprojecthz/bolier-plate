0502815f074826b7ee46ed8d68526891ff0e4cdf
TANGRAM <tangram@eunsub-iMac.local>
2022-05-17 11:27:40 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+const {
+    User
+} = require("./models/Uesr");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hz:rhdvkf21@bolierplate.cvtvm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MONGODB Connected')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI).then(() => console.log('MONGODB Connected')).catch(err => console.log(err))
 
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('Hello');
 });
+
+app.post("/register", (req, res) => {
+    //회원가입할때 필요한 정보들을 클라이언트에서 가져오면 그것들을 디비에 넣어준다.
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({
+            success: false,
+            err
+        })
+        return res.status(200).json({
+            success: true
+        })
+
+    })
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`))
